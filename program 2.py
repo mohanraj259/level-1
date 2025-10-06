255ed449d6ccb820772201f02229ead5829e8f02
mohanraj259 <mohanrajmohan6507@gmail.com>
2025-10-06 10:44:23 +0530

    program 2

---
+++ program 2.py
@@ -0,0 +1,14 @@
+print("takshashila university")
+print("ongur,tindivanam,villupuram dist,tn")
+print("__________________")
+print("STUDENT MARK LIST")
+print("__________________")
+str1=input("enter the register no:")
+str2=input("enter the name:")
+m1=int(input("python mark:"))
+m2=int(input("dbms mark:"))
+m3=int(input("finical accounting:"))
+total=m1+m2+m3
+print("total mark=",total)
+avg=total/3
+print("average mark=",avg)       
