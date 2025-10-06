ece77a1fce9ff1061ff16d4877d3d249b3732988
mohanraj259 <mohanrajmohan6507@gmail.com>
2025-10-06 10:45:09 +0530

    program 3

---
+++ porgram3.py
@@ -0,0 +1,11 @@
+print("goverment of tamilnadu")
+print("electricity board")
+print("__________________")
+no1=input("enter the eb-no:020-030-1234")
+no2=input("enter the customer name:")
+pre=int(input("reading for previous math:"))
+cur=int(input("readimg for covert math:"))
+total=cur-pre
+print("total uint cusumed:",total)
+paid=total*5
+print("amount to be paid rs:",paid)
